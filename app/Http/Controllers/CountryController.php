fc6e990c343c47417a126f96d0240f5b0a9c1592
lego-dego <tahashahid292@gmail.com>
2019-10-15 13:00:01 +0500

    api's new episodes

---
+++ app/Http/Controllers/CountryController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Country;
+use Illuminate\Http\Request;
+
+class CountryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(\App\Country::all() , 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+            $country = \App\Country::create($request->all());
+            return response()->json($country , 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Country  $country
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Country $country)
+    {
+        return response()->json($country , 200  );
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Country  $country
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Country $country)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Country  $country
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $country = \App\Country::find($id);
+        if(!empty($country)){
+            $country->name = $request->name;
+            $country->save();
+            return response()->json($country, 200);
+
+        }
+        else{
+            return response()->json('Country not Found' , 400); 
+        }
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Country  $country
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $country = \App\Country::destroy($id);
+
+        return response()->json(\App\Country::all() , 200); 
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('country' , 'CountryController@index');
+Route::get('country/{country}' , 'CountryController@show');
+Route::post('save' , 'CountryController@store');
+Route::get('country/{id}/destroy' , 'CountryController@destroy');
+Route::post('country/{id}/edit' , 'CountryController@update');
